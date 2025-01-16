7648acebef6e6a0fdcff41548b17a4b998ee4d5a
davdapritam <dpdavdapritam@gmail.com>
2025-01-16 23:07:16 +0530

    refract

--- src/app/core/handlers/user-handler.ts
+++ src/app/core/handlers/user-handler.ts
@@ -1,7 +1,7 @@
-import {Injectable} from "@angular/core";
-import {getUserError, getUserLoaded, getUserLoading, getUsers, RootReducerState} from "../reducers";
-import {Store} from "@ngrx/store";
-import {combineLatest, Observable, take} from "rxjs";
+import { Injectable } from "@angular/core";
+import { getUserError, getUserLoaded, getUserLoading, getUsers, RootReducerState } from "../reducers";
+import { Store } from "@ngrx/store";
+import { combineLatest, Observable, take } from "rxjs";
 import {
   UserAddAction,
   UserDeleteAction,
@@ -10,8 +10,8 @@ import {
   UserListSuccessAction,
   UserUpdateAction
 } from "../actions/user-action";
-import {UserService} from "../../services/user.service";
-import {User} from "../../common/models/user";
+import { UserService } from "../../services/user.service";
+import { User } from "../../common/models/user";
 
 @Injectable()
 export class UserHandler {
@@ -28,7 +28,7 @@ export class UserHandler {
       if ((!data[0] && !data[1]) || force) {
         this.store.dispatch(new UserListRequestAction());
         this.userService.getUsers().subscribe(res => {
-          this.store.dispatch(new UserListSuccessAction({data: res}));
+          this.store.dispatch(new UserListSuccessAction({ data: res }));
         }, error => {
           this.store.dispatch(new UserListErrorAction());
         })
@@ -44,20 +44,20 @@ export class UserHandler {
 
   addUser(data: User) {
     this.userService.addUser(data).subscribe((res) => {
-      this.store.dispatch(new UserAddAction({data: res}))
+      this.store.dispatch(new UserAddAction({ data: res }))
     });
   }
 
   updateUser(data: User) {
     this.userService.updateUser(data).subscribe((res) => {
-      this.store.dispatch(new UserUpdateAction({data: res}))
+      this.store.dispatch(new UserUpdateAction({ data: res }))
     })
   }
 
   deleteUser(userId: string) {
     this.userService.deleteUser(userId).subscribe((res) => {
       if (res.statusCode === 200) {
-        this.store.dispatch(new UserDeleteAction({id: userId}))
+        this.store.dispatch(new UserDeleteAction({ id: userId }))
       }
     });
   }
